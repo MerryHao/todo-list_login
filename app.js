2b1d215054bf05790f6bac8dffaa3b3fcf8adf9c
MerryHao <mq6721@gmail.com>
2023-01-24 15:24:01 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.use(session({
   saveUninitialized: true //強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session
 }))
 usePassport(app)
+app.use((req, res, next) => {
+  console.log(req.user) //觀察用
+  res.locals.isAuthenticated = req.isAuthenticated() //把 req.isAuthenticated() 回傳的布林值，交接給 res 使用
+  res.locals.user = req.user //把使用者資料交接給 res 使用
+  next()
+})
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method')) // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(routes) // 將 request 導入路由器。引入路由器時，路徑設定為 /routes 就會自動去尋找目錄下叫做 index 的檔案。
