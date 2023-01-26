0ee259a868e5048c70a47e2eb2327c27772e1a36
MerryHao <mq6721@gmail.com>
2023-01-26 19:21:31 +0800

    feat: modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,9 +1,35 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Todo = require('../todo') // 載入 todo model
 const db = require('../../config/mongoose')
-
+const user = require('../user')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: 'name-' + i })
-  }
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => user.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Todo.create({ name: `name-${i}`, userId })
+      ))
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit() //關閉這段Node執行程序，由於這段seeder程式只有在第一次初始化時才會用到，
+      //不像專案主程式一旦開始就執行運作，所以在seeder做好以後把這個臨時的Node.js程序結束掉。
+      //類似把臨時的Node.js執行環境關機的概念
+    })
 })
